e533dd1a478de049cc752cda82dde3378fdcf284
Nachin1998 <Ignacio.f.l@hotmail.com>
2019-10-15 19:17:13 -0300

    Main menu UI finished

--- Project1/mainMenu.cpp
+++ Project1/mainMenu.cpp
@@ -1,37 +1,62 @@
 #include "mainMenu.h"
-#include "raylib.h"
 #include "gameManager.h"
+#include <iostream>
+
+Button buttons[cant];
+
+void setButtons() {
+
+	for (int i = 0; i < cant; i++)
+	{
+		buttons[i].rec.x = 20;
+		buttons[i].rec.height = 24;
+		buttons[i].color = LIGHTGRAY;
+	}
+
+	buttons[0].rec.y = screenHeight / 2 - 60;  //Play button
+	buttons[0].rec.width = 50;
+
+	buttons[1].rec.y = screenHeight / 2 - 15;  //Controls button
+	buttons[1].rec.width = 105;
+
+	buttons[2].rec.y = screenHeight / 2 + 30;  //Credits button
+	buttons[2].rec.width = 90;
+
+	buttons[3].rec.y = screenHeight / 2 + 75;  //Exit button
+	buttons[3].rec.width = 115;
+
+}
 
 void mainMenu() {
 
-	Rectangle playButton;
-	playButton.x = 20;
-	playButton.y = screenHeight / 2 - 60;
-	playButton.height = 24;
-	playButton.width = 50;
 	SetTargetFPS(60);
-
+	setButtons();
 	
 	while (!WindowShouldClose())
 	{
-		
 		BeginDrawing();
 
 		ClearBackground(BLACK);
 
-		DrawText("Nark-anoid", 20, screenHeight / 2 - 200, 50, LIGHTGRAY);
-		//DrawRectangleRec(playButton, WHITE);
-		if (!CheckCollisionPointRec(GetMousePosition(), playButton))
-			DrawText("Play", 20, screenHeight / 2 - 60, 24, LIGHTGRAY);
-		else
-			DrawText("Play", 20, screenHeight / 2 - 60, 24, BLUE);
+		DrawText("Nark-anoid", buttons[0].rec.x, screenHeight / 2 - 200, 50, LIGHTGRAY);
+		DrawText("Play", buttons[0].rec.x, buttons[0].rec.y, buttons[0].rec.height, buttons[0].color);
+		DrawText("Controls", buttons[1].rec.x, buttons[1].rec.y, buttons[1].rec.height, buttons[1].color);
+		DrawText("Credits", buttons[2].rec.x, buttons[2].rec.y, buttons[2].rec.height, buttons[2].color);
+		DrawText("Exit game", buttons[3].rec.x, buttons[3].rec.y, buttons[3].rec.height, buttons[3].color);
 
-		if (CheckCollisionPointRec(GetMousePosition(), playButton) && IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-			DrawText("Play", 20, screenHeight / 2 - 60, 24, DARKBLUE);
+		//Color change
+		for (int i = 0; i < cant; i++)
+		{
+			if (!CheckCollisionPointRec(GetMousePosition(), buttons[i].rec))
+				buttons[i].color = LIGHTGRAY;
+			else
+				buttons[i].color = BLUE;
+
+			if (CheckCollisionPointRec(GetMousePosition(), buttons[i].rec) && IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+				buttons[i].color = DARKBLUE;
+				if (i == 3)exit(0);
+			}
 		}
-		DrawText("Controls", 60 - 40, screenHeight / 2 - 15, 24, LIGHTGRAY);
-		DrawText("Credits", 60 - 40, screenHeight / 2 + 30, 24, LIGHTGRAY);
-		DrawText("Exit", 60 - 40, screenHeight / 2 + 75, 24, LIGHTGRAY);
 
 		DrawText("P to pause", GetScreenWidth() / 2 - 60, screenHeight / 2 + 200, 20, LIGHTGRAY);
 
--- Project1/mainMenu.h
+++ Project1/mainMenu.h
@@ -1,6 +1,17 @@
 #ifndef MENU_H
 #define MENU_H
+#include "raylib.h"
 
+const int cant = 4;
+
+struct Button {
+	Rectangle rec;
+	Color color;
+};
+
+extern Button buttons[cant];
+
+void setButtons();
 void mainMenu();
 
 #endif
